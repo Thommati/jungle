6a27c3e284e51785657ae9c73909b7b5efe00b94
Matthew Thompson <supra__charged@hotmail.com>
2021-06-24 20:12:06 +0000

    Add login and log out controllers, actions, and views

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,13 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources  :categories, only: [:index, :new, :create]
   end
-
+  
   get 'about' => 'about#index'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
   get 'signup' => 'users#new'
   post 'users' => 'users#create'
 
