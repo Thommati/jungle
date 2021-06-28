7cf714a819537bdb4ce364ba264b186bd432ade6
Matthew Thompson <supra__charged@hotmail.com>
2021-06-28 22:10:38 +0000

    Add minimum length requirement for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :password, length: { minimum: 4 }
 end
